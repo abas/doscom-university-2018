ff01371721f17374d6f4d0052419aa289918831f
abas <akhmadbasir5@gmail.com>
2018-03-05 14:09:36 +0700

    redirect to admin

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return redirect('/admin/v65sv87s5rd6vts7vr6s5fdv7sf5v67sdv765sd6v7');
         }
 
         return $next($request);
