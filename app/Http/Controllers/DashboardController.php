4d666f7576ce94bcc2f13463f5cfc6a89da7afff
ahmadbasir <akhmadbasir5@gmail.com>
2018-03-16 01:44:39 +0700

    add var for transaksi

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,7 +13,34 @@ class DashboardController extends Controller
     {
         $kelas = Kelas::all();
         $peserta = Peserta::all();
-
-        return view('admin.dashboard',compact('kelas','peserta'));
+        // return $peserta;
+        $lunas = Peserta::where('status_pembayaran','lunas')->count();
+        $total_uang_tekumpul = 0;
+        foreach ($peserta as $p) {
+            if($p->status_pembayaran == 'lunas'){
+                if($p->web == 'true'){
+                    $total_uang_tekumpul += 35000;
+                }
+                if($p->femdev == 'true'){
+                    $total_uang_tekumpul += 35000;
+                }
+                if($p->mobile == 'true'){
+                    $total_uang_tekumpul += 35000;
+                }
+                if($p->linux == 'true'){
+                    $total_uang_tekumpul += 35000;
+                }
+                if($p->net == 'true'){
+                    $total_uang_tekumpul += 35000;
+                }
+                if($p->inkscape == 'true'){
+                    $total_uang_tekumpul += 35000;
+                }
+                if($p->godot == 'true'){
+                    $total_uang_tekumpul += 35000;
+                }
+            }
+        }
+        return view('admin.dashboard',compact('kelas','peserta','lunas','total_uang_tekumpul'));
     }
 }
