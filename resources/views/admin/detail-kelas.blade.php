e61b4a2825e9a03cdc126ab3b2a71e3fbda5bb0a
ahmadbasir <akhmadbasir5@gmail.com>
2018-03-16 07:27:08 +0700

    may last update! by @abas

--- resources/views/admin/detail-kelas.blade.php
+++ resources/views/admin/detail-kelas.blade.php
@@ -4,32 +4,56 @@
         <div class="card-header">
             <div class="card-title">
                 <h1>
-                <a class="btn btn-sm btn-raised btn-success" href="{{route('dashboard')}}"><<</a>
-                Peserta</h1>
+                    <a class="btn btn-sm btn-raised btn-success" href="{{route('dashboard')}}">
+                        <<</a>
+                            Peserta</h1>
             </div>
         </div>
         <div class="card-body text-center">
             <div class="row">
-                @foreach($peserta as $peserta)
-                @if($peserta->status_pembayaran == 'lunas')
-                <div class="col-md-3">
-                    <div class=" card panel-default">
-                        <div class="panel-body">
-                            <h3 style="text-transform:capitalize">
-                                <b>
-                                    {{$peserta->nama}}
-                                </b>
-                            </h3>
-                        </div>
-                        <div class="panel-heading" class="bg-info">
-                            <b>
 
-                            </b>
+                <div id="admin" class="col-md-12">
+                    <div class="card material-table">
+                        <div class="table-header">
+                            <span class="table-title">Total Semua Peserta :
+                                <b></b>
+                            </span>
                         </div>
+                        <table id="datatable" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th>Nama</th>
+                                    <th>Email</th>
+                                    <th>Nomor Hp</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tfoot>
+                                <tr>
+                                    <th>Nama</th>
+                                    <th>Email</th>
+                                    <th>Nomor Hp</th>
+                                    <th>Action</th>
+                                </tr>
+                            </tfoot>
+                            <tbody>
+                                @foreach($peserta as $peserta)
+                                <tr style="text-align:left">
+                                    <td>{{$peserta->nama}}</td>
+                                    <td>{{$peserta->email}}</td>
+                                    <td>{{$peserta->nohp}}</td>
+                                    <td style="width:3%">
+                                        <a href="{{route('delete-peserta',$peserta->id)}}" class="btn btn-sm btn-raised btn-danger">
+                                            <b>kick</b>
+                                        </a>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-                @endif
-                @endforeach
+
             </div>
         </div>
     </div>
